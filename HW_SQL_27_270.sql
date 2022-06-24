d69409daa4a21edb93609be03dd1c8046142e788
Yurbt <99122269+Yurbt@users.noreply.github.com>
2022-06-24 12:47:52 +1100

    Add files via upload

---
+++ HW_SQL_27_270.sql
@@ -0,0 +1,79 @@
+
+create table employees_27_270(id serial primary key, employee_name varchar(50) not null);
+insert into employees_27_270(id, employee_name) values ('1','Иванов Иван Иванович') ;
+insert into employees_27_270(id, employee_name) values ('2','Иванов Иван Сергеевич'); 
+insert into employees_27_270(id, employee_name) values ('3','Иванов Иван Андреевич'); 
+insert into employees_27_270(id, employee_name) values ('4','Петров Иван Иванович');
+insert into employees_27_270(id, employee_name) values ('5','Перов Иван Иванович'); 
+insert into employees_27_270(id, employee_name) values ('6','Диванов Сергей Иванович');
+insert into employees_27_270(id, employee_name) values ('7','Иванова Ирина Ивановна'); 
+insert into employees_27_270(id, employee_name) values ('8','Ионов Борис Павлович'); 
+insert into employees_27_270(id, employee_name) values ('9','Трушкин Павел Петрович') ;
+insert into employees_27_270(id, employee_name) values ('10','Светлова Елена Сергеевно') ;
+insert into employees_27_270(id, employee_name) values ('11','Тимофеева Лариса Петровна'); 
+insert into employees_27_270(id, employee_name) values ('12','Пельмешкина Арина Родионовна');
+insert into employees_27_270(id, employee_name) values ('13','Тюфтий Виктор Евгеньевич'); 
+insert into employees_27_270(id, employee_name) values ('14','Толстой Лев Иванович'); 
+insert into employees_27_270(id, employee_name) values ('15','Блохин Олег Иванович') ;
+insert into employees_27_270(id, employee_name) values ('16','Бесков Владимир Ильич') ;
+insert into employees_27_270(id, employee_name) values ('17','Брежнева Вера Петровна'); 
+insert into employees_27_270(id, employee_name) values ('18','Лобода Светлана Георгиевна');
+insert into employees_27_270(id, employee_name) values ('19','Киркоров Глеб Андреевич'); 
+insert into employees_27_270(id, employee_name) values ('20','Пугачев Емельян Петрович'); 
+insert into employees_27_270(id, employee_name) values ('21','Разин Степан Иванович'); 
+insert into employees_27_270(id, employee_name) values ('22','Разин Андрей Петрович'); 
+insert into employees_27_270(id, employee_name) values ('23','Дзюба Валерий Павлович'); 
+insert into employees_27_270(id, employee_name) values ('24','Гагарин Юрий Алексеевич');
+insert into employees_27_270(id, employee_name) values ('25','Булгаков Анатолий Яковлевич'); 
+insert into employees_27_270(id, employee_name) values ('26','Шолохов Леонид Ильич') ;
+insert into employees_27_270(id, employee_name) values ('27','Ленин Автандил Автондилович') ;
+insert into employees_27_270(id, employee_name) values ('28','Березкин Иван Иванович'); 
+insert into employees_27_270(id, employee_name) values ('29','Береза Светлана Ивановна') ;
+insert into employees_27_270(id, employee_name) values ('30','Дубина Ольга Петровна');
+insert into employees_27_270(id, employee_name) values ('31','Яковенко Сергей Сергеевич') ;
+insert into employees_27_270(id, employee_name) values ('32','Сикорский Андрей Григорьевич') ;
+insert into employees_27_270(id, employee_name) values ('33','Чапаев Василий Иванович') ;
+insert into employees_27_270(id, employee_name) values ('34','Круг Ирина Петровна') ;
+insert into employees_27_270(id, employee_name) values ('35','Киут Иван Иванович') ;
+insert into employees_27_270(id, employee_name) values ('36','Иус Ольга Ивановна');
+insert into employees_27_270(id, employee_name) values ('37','Корк Петр Иванович'); 
+insert into employees_27_270(id, employee_name) values ('38','Ра Бог Египетский') ;
+insert into employees_27_270(id, employee_name) values ('39','Бек Абдула Петрович') ;
+insert into employees_27_270(id, employee_name) values ('40','Шольц Олаф Иванович') ;
+insert into employees_27_270(id, employee_name) values ('41','Реут Олеся Сергеевна') ;
+insert into employees_27_270(id, employee_name) values ('42','Кук Андрей Михайлович');
+insert into employees_27_270(id, employee_name) values ('43','Смит Адам Иванович') ;
+insert into employees_27_270(id, employee_name) values ('44','Рид Джон Петрович') ;
+insert into employees_27_270(id, employee_name) values ('45','Ли Харви Освальд') ;
+insert into employees_27_270(id, employee_name) values ('46','Ри Крис Иванович') ;
+insert into employees_27_270(id, employee_name) values ('47','Грин Бакс Иванович') ;
+insert into employees_27_270(id, employee_name) values ('48','Янг Бойс Сидорович');
+insert into employees_27_270(id, employee_name) values ('49','Райт Петр Петрович') ;
+insert into employees_27_270(id, employee_name) values ('50','Картер Джон Сергеевич') ;
+insert into employees_27_270(id, employee_name) values ('51','Байден Иван Джозефович') ;
+insert into employees_27_270(id, employee_name) values ('52','Трамп Дмитрий Иванович') ;
+insert into employees_27_270(id, employee_name) values ('53','Лопес Джулия Константиновна') ;
+insert into employees_27_270(id, employee_name) values ('54','Мур Демми Михайловна');
+insert into employees_27_270(id, employee_name) values ('55','Кокс Иван Иванович') ;
+insert into employees_27_270(id, employee_name) values ('56','Диас Катерина Павловна') ;
+insert into employees_27_270(id, employee_name) values ('57','Крез Борис Алексеевич') ;
+insert into employees_27_270(id, employee_name) values ('58','Губкин Михаил Васильевич') ;
+insert into employees_27_270(id, employee_name) values ('59','Дрозд Николай Иванович') ;
+insert into employees_27_270(id, employee_name) values ('60','Горбачев Михаил Валерианович');
+insert into employees_27_270(id, employee_name) values ('61','Сталин Василий Григорьевич') ;
+insert into employees_27_270(id, employee_name) values ('62','Белов Петр Петрович') ;
+insert into employees_27_270(id, employee_name) values ('63','Дрозд Екатерина Алексеевна') ;
+insert into employees_27_270(id, employee_name) values ('64','Хряк Олег Евгеньевич') ;
+insert into employees_27_270(id, employee_name) values ('65','Пупкин Иван Иванович') ;
+insert into employees_27_270(id, employee_name) values ('66','Задов Лев Иванович');
+insert into employees_27_270(id, employee_name) values ('67','Васькин Леон Иванович') ;
+insert into employees_27_270(id, employee_name) values ('68','Акеров Ровшан Петрович') ;
+insert into employees_27_270(id, employee_name) values ('69','Иванова Нонна Геннадьевна') ;
+insert into employees_27_270(id, employee_name) values ('70','Моськин Слон Андреевич') ;
+
+
+
+
+
+
+select * from employees_27_270
