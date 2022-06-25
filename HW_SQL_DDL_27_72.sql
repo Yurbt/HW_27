6199b1900ce00ca4e789f5142c4a43062da7868f
Yurbt <99122269+Yurbt@users.noreply.github.com>
2022-06-25 11:28:51 +1100

    ddl

---
+++ HW_SQL_DDL_27_72.sql
@@ -0,0 +1,226 @@
+Таблица employees
+
+Создать таблицу employees
+- id. serial,  primary key,
+- employee_name. Varchar(50), not null
+Наполнить таблицу employee 70 строками.
+
+
+
+
+
+--create table emrloyees_27_72(id serial rrimary key, emrloyee_name varchar(50) not null);
+--insert into emrloyees_27_72(id, emrloyee_name) values ('1','ivanov ivan ivanovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('2','ivanov ivan sergeevich'); 
+--insert into emrloyees_27_72(id, emrloyee_name) values ('3','ivanov ivan andreevich'); 
+--insert into emrloyees_27_72(id, emrloyee_name) values ('4','petrov ivan ivanovich');
+--insert into emrloyees_27_72(id, emrloyee_name) values ('5','perov ivan ivanovich'); 
+--insert into emrloyees_27_72(id, emrloyee_name) values ('6','divanov sergey ivanovich');
+--insert into emrloyees_27_72(id, emrloyee_name) values ('7','ivanova irina ivanovna'); 
+--insert into emrloyees_27_72(id, emrloyee_name) values ('8','ionov boris pavlovich'); 
+--insert into emrloyees_27_72(id, emrloyee_name) values ('9','trushkin pavel petrovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('10','svetlova elena sergeevno') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('11','timoфeeva larisa petrovna'); 
+--insert into emrloyees_27_72(id, emrloyee_name) values ('12','pelmeshkina arina rodionovna');
+--insert into emrloyees_27_72(id, emrloyee_name) values ('13','tyuфtiy viktor evgenevich'); 
+--insert into emrloyees_27_72(id, emrloyee_name) values ('14','tolstoy lev ivanovich'); 
+--insert into emrloyees_27_72(id, emrloyee_name) values ('15','blokhin oleg ivanovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('16','beskov vladimir ilich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('17','brezhneva vera petrovna'); 
+--insert into emrloyees_27_72(id, emrloyee_name) values ('18','loboda svetlana georgievna');
+--insert into emrloyees_27_72(id, emrloyee_name) values ('19','kirkorov gleb andreevich'); 
+--insert into emrloyees_27_72(id, emrloyee_name) values ('20','pugachev emelyan petrovich'); 
+--insert into emrloyees_27_72(id, emrloyee_name) values ('21','razin stepan ivanovich'); 
+--insert into emrloyees_27_72(id, emrloyee_name) values ('22','razin andrey petrovich'); 
+--insert into emrloyees_27_72(id, emrloyee_name) values ('23','dzyuba valeriy pavlovich'); 
+--insert into emrloyees_27_72(id, emrloyee_name) values ('24','gagarin yuriy alekseevich');
+--insert into emrloyees_27_72(id, emrloyee_name) values ('25','bulgakov anatoliy yakovlevich'); 
+--insert into emrloyees_27_72(id, emrloyee_name) values ('26','sholokhov leonid ilich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('27','lenin avtandil avtondilovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('28','berezkin ivan ivanovich'); 
+--insert into emrloyees_27_72(id, emrloyee_name) values ('29','bereza svetlana ivanovna') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('30','dubina olga petrovna');
+--insert into emrloyees_27_72(id, emrloyee_name) values ('31','yakovenko sergey sergeevich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('32','sikorskiy andrey grigorevich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('33','chapaev vasiliy ivanovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('34','krug irina petrovna') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('35','kiut ivan ivanovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('36','ius olga ivanovna');
+--insert into emrloyees_27_72(id, emrloyee_name) values ('37','kork petr ivanovich'); 
+--insert into emrloyees_27_72(id, emrloyee_name) values ('38','ra bog egipetskiy') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('39','bek abdula petrovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('40','sholc olaф ivanovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('41','reut olesya sergeevna') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('42','kuk andrey mikhaylovich');
+--insert into emrloyees_27_72(id, emrloyee_name) values ('43','smit adam ivanovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('44','rid dzhon petrovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('45','li kharvi osvald') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('46','ri kris ivanovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('47','grin baks ivanovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('48','yang boys sidorovich');
+--insert into emrloyees_27_72(id, emrloyee_name) values ('49','rayt petr petrovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('50','karter dzhon sergeevich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('51','bayden ivan dzhozeфovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('52','tramp dmitriy ivanovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('53','lopes dzhuliya konstantinovna') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('54','mur demmi mikhaylovna');
+--insert into emrloyees_27_72(id, emrloyee_name) values ('55','koks ivan ivanovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('56','dias katerina pavlovna') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('57','krez boris alekseevich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('58','gubkin mikhail vasilevich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('59','drozd nikolay ivanovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('60','gorbachev mikhail valerianovich');
+--insert into emrloyees_27_72(id, emrloyee_name) values ('61','stalin vasiliy grigorevich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('62','belov petr petrovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('63','drozd ekaterina alekseevna') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('64','khryak oleg evgenevich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('65','pupkin ivan ivanovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('66','zadov lev ivanovich');
+--insert into emrloyees_27_72(id, emrloyee_name) values ('67','vaskin leon ivanovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('68','akerov rovshan petrovich') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('69','ivanova nonna gennadevna') ;
+--insert into emrloyees_27_72(id, emrloyee_name) values ('70','moskin slon andreevich') ;
+--
+--
+--
+--
+--
+--
+--select * from emrloyees_27_72
+
+Таблица salary
+
+Создать таблицу salary
+- id. Serial  primary key,
+- monthly_salary. Int, not null
+Наполнить таблицу salary 15 строками:
+- 1000
+- 1100
+- 1200
+- 1300
+- 1400
+- 1500
+- 1600
+- 1700
+- 1800
+- 1900
+- 2000
+- 2100
+- 2200
+- 2300
+- 2400
+- 2500
+
+
+
+
+
+--create table salary_27_72(id Serial primary key, monthly_salary int not null);
+--insert into salary_27_72 (monthly_salary) values(1000);
+--insert into salary_27_72 (monthly_salary) values(1100);
+--insert into salary_27_72 (monthly_salary) values(1200);
+--insert into salary_27_72 (monthly_salary) values(1300);
+--insert into salary_27_72 (monthly_salary) values(1400);
+--insert into salary_27_72 (monthly_salary) values(1500);
+--insert into salary_27_72 (monthly_salary) values(1600);
+--insert into salary_27_72 (monthly_salary) values(1700);
+--insert into salary_27_72 (monthly_salary) values(1800);
+--insert into salary_27_72 (monthly_salary) values(1900);
+--insert into salary_27_72 (monthly_salary) values(2000);
+--insert into salary_27_72 (monthly_salary) values(2100);
+--insert into salary_27_72 (monthly_salary) values(2200);
+--insert into salary_27_72 (monthly_salary) values(2300);
+--insert into salary_27_72 (monthly_salary) values(2400);
+--insert into salary_27_72 (monthly_salary) values(2500);
+--
+--
+--select * from salary_27_72;
+
+
+Таблица employee_salary
+
+Создать таблицу employee_salary
+- id. Serial  primary key,
+- employee_id. Int, not null, unique
+- salary_id. Int, not null
+Наполнить таблицу employee_salary 40 строками:
+- в 10 строк из 40 вставить несуществующие employee_id
+
+
+
+
+--DROP TABLE IF EXISTS employee_salary_27_72;
+--create table employee_salary_27_72(id Serial primary key, employee_id int not null unique, salary_id int not null);
+--insert into employee_salary_27_72(employee_id, salary_id) values (3,7);
+--insert into employee_salary_27_72(employee_id, salary_id) values (1,4);
+--insert into employee_salary_27_72(employee_id, salary_id) values (5,9);
+--insert into employee_salary_27_72(employee_id, salary_id) values (40,13);
+--insert into employee_salary_27_72(employee_id, salary_id) values (23,4);
+--insert into employee_salary_27_72(employee_id, salary_id) values (11,2);
+--insert into employee_salary_27_72(employee_id, salary_id) values (52,10);
+--insert into employee_salary_27_72(employee_id, salary_id) values (15,13);
+--insert into employee_salary_27_72(employee_id, salary_id) values (26,4);
+--insert into employee_salary_27_72(employee_id, salary_id) values (16,1);
+--insert into employee_salary_27_72(employee_id, salary_id) values (33,7);
+--insert into employee_salary_27_72(employee_id, salary_id) values (300,7);
+--insert into employee_salary_27_72(employee_id, salary_id) values (86,14);
+--insert into employee_salary_27_72(employee_id, salary_id) values (2,8);
+--insert into employee_salary_27_72(employee_id, salary_id) values (4,10);
+--insert into employee_salary_27_72(employee_id, salary_id) values (6,11);
+--insert into employee_salary_27_72(employee_id, salary_id) values (7,10);
+--insert into employee_salary_27_72(employee_id, salary_id) values (69,7);
+--insert into employee_salary_27_72(employee_id, salary_id) values (65,8);
+--insert into employee_salary_27_72(employee_id, salary_id) values (50,7);
+--insert into employee_salary_27_72(employee_id, salary_id) values (51,15);
+--insert into employee_salary_27_72(employee_id, salary_id) values (54,7);
+--insert into employee_salary_27_72(employee_id, salary_id) values (41,7);
+--insert into employee_salary_27_72(employee_id, salary_id) values (73,12);
+--insert into employee_salary_27_72(employee_id, salary_id) values (93,8);
+--insert into employee_salary_27_72(employee_id, salary_id) values (70,1);
+--insert into employee_salary_27_72(employee_id, salary_id) values (45,2);
+--insert into employee_salary_27_72(employee_id, salary_id) values (46,2);
+--insert into employee_salary_27_72(employee_id, salary_id) values (37,9);
+--insert into employee_salary_27_72(employee_id, salary_id) values (85,7);
+--insert into employee_salary_27_72(employee_id, salary_id) values (103,7);
+--insert into employee_salary_27_72(employee_id, salary_id) values (31,5);
+--insert into employee_salary_27_72(employee_id, salary_id) values (411,7);
+--insert into employee_salary_27_72(employee_id, salary_id) values (32,7);
+--insert into employee_salary_27_72(employee_id, salary_id) values (113,9);
+--insert into employee_salary_27_72(employee_id, salary_id) values (34,4);
+--insert into employee_salary_27_72(employee_id, salary_id) values (36,6);
+--insert into employee_salary_27_72(employee_id, salary_id) values (105,7);
+--insert into employee_salary_27_72(employee_id, salary_id) values (39,7);
+--insert into employee_salary_27_72(employee_id, salary_id) values (211,11);
+--
+--select * from employee_salary_27_72
+
+
+Таблица roles
+
+Создать таблицу roles
+- id. Serial  primary key,
+- role_name. int, not null, unique
+Поменять тип столба role_name с int на varchar(30)
+Наполнить таблицу roles 20 строками:
+
+
+
+
+
+
+
+--create table roles_27_72 (id Serial primary key, role_name int not null unique);
+--alter table roles_27_72
+--alter column role_name TYPE varchar(30);
+
+
+
+
+Таблица roles_employee
+
+Создать таблицу roles_employee
+- id. Serial  primary key,
+- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
+- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+Наполнить таблицу roles_employee 40 строками:
+
